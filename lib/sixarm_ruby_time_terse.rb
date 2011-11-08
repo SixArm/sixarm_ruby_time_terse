98cc1138fb5ee426216bda467f77f38ac8be68bb
SixArm <sixarm@sixarm.com>
2011-11-07 19:14:17 -0800

    Publish

---
+++ lib/sixarm_ruby_time_terse.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+=begin rdoc
+Please see README.rdoc
+=end
+
+
+class Time
+
+
+  # @return [String] time packed into a short string: "YYYYMMDDHHMMSS"
+  #
+  # The time is converted to UTC.
+  #
+  # @example
+  #   Time.now.terse 
+  #   => "20101231125959" 
+
+  def terse
+    getutc.strftime('%Y%m%d%H%M%S')
+  end
+
+
+  # Shorthand for Time.now.terse
+  #
+  # @example
+  #   Time.terse
+  #    => "20101231125959" 
+  #
+  # @return [String] Time.now.terse
+
+  def self.terse
+    now.terse
+  end
+
+
+end
---
+++ test/sixarm_ruby_time_terse_test.rb
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+require 'test/unit'
+require 'simplecov'
+SimpleCov.start
+require 'sixarm_ruby_time_terse'
+
+class TimeTest < Test::Unit::TestCase
+
+ def test_terse_with_class_method
+   t=Time.terse
+   assert(t=~/^\d\d\d\d\d\d\d\d\d\d\d\d\d\d$/,t)
+ end
+
+ def test_terse_with_instance_method
+   t=Time.now.terse
+   assert(t=~/^\d\d\d\d\d\d\d\d\d\d\d\d\d\d$/,t)
+ end
+ 
+end
+
