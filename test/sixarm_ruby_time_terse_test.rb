88bfc6fffa87d25f5c775767ea5a2354b144ff7e
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:57 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_time_terse_test.rb
+++ test/sixarm_ruby_time_terse_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_time_terse'
+require "sixarm_ruby_time_terse"
 
 describe Time do
 
