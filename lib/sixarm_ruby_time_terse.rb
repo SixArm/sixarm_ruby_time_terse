68411c7e74bfc1d340293908563ec0f931c54198
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-18 23:14:31 -0600

    Fix require

--- lib/sixarm_ruby_time_terse.rb
+++ lib/sixarm_ruby_time_terse.rb
@@ -3,34 +3,4 @@
 Please see README
 =end
 
-
-class Time
-
-
-  # @return [String] time packed into a short string: "YYYYMMDDHHMMSS"
-  #
-  # The time is converted to UTC.
-  #
-  # @example
-  #   Time.now.terse 
-  #   => "20101231125959" 
-
-  def terse
-    getutc.strftime('%Y%m%d%H%M%S')
-  end
-
-
-  # Shorthand for Time.now.terse
-  #
-  # @example
-  #   Time.terse
-  #    => "20101231125959" 
-  #
-  # @return [String] Time.now.terse
-
-  def self.terse
-    now.terse
-  end
-
-
-end
+require_relative "sixarm_ruby_time_terse/time.rb"
